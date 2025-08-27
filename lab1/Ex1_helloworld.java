17fb655f984e312c8e004e668f9e20338bdcacaf
Andrew <aharroo13@gmail.com>
2025-08-27 00:37:29 +0000

    save helloworld

---
+++ lab1/Ex1_helloworld.java
@@ -0,0 +1,12 @@
+package lab1;
+public class Ex1_helloworld { 
+    public static void main(String[]args ) { 
+        System.out.print("this is not new line");
+        System.out.println("hello world");
+        System.out.print("this is not new line");
+
+    }
+
+
+
+}
